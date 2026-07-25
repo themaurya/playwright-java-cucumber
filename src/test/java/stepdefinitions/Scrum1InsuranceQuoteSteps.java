c69a3f973809a5731424f185a0c34fd0a7dbbd0b
Sanjay Maurya <90902275+themaurya@users.noreply.github.com>
2026-07-25 15:43:17 +0530

    Remove duplicate navigation step from SCRUM-1 steps

--- src/test/java/stepdefinitions/Scrum1InsuranceQuoteSteps.java
+++ src/test/java/stepdefinitions/Scrum1InsuranceQuoteSteps.java
@@ -1,7 +1,6 @@
 package stepdefinitions;
 
 import factory.DriverFactory;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
@@ -10,17 +9,12 @@ import pages.Scrum1InsuranceQuotePage;
 public class Scrum1InsuranceQuoteSteps {
     Scrum1InsuranceQuotePage scrum1InsuranceQuotePage = new Scrum1InsuranceQuotePage(DriverFactory.getPage());
 
-    @Given("^user navigates to \"([^\"]*)\"$")
-    public void navigateToUrl(String url) {
-        scrum1InsuranceQuotePage.navigateToUrl(url);
-    }
-
-    @When("^user enters \"([^\"]*)\" vehicle number in Two Wheeler Insurance Quote page$")
+    @When("^user enters \"([^"]*)\" vehicle number in Two Wheeler Insurance Quote page$")
     public void enterVehicleNumber(String vehicleNumber) {
         scrum1InsuranceQuotePage.enterVehicleNumber(vehicleNumber);
     }
 
-    @When("^user enters \"([^\"]*)\" mobile number in Two Wheeler Insurance Quote page$")
+    @When("^user enters \"([^"]*)\" mobile number in Two Wheeler Insurance Quote page$")
     public void enterMobileNumber(String mobileNumber) {
         scrum1InsuranceQuotePage.enterMobileNumber(mobileNumber);
     }
